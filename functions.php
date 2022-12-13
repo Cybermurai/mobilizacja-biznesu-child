5028f0b5dc1897722f018f57721baa71afb42878
Cybermurai <github@cybermurai.com>
2022-12-13 09:07:41 +0100

    functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-	$parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	$parenthandle = 'parent-style';
 	$theme        = wp_get_theme();
 	wp_enqueue_style( $parenthandle,
 		get_template_directory_uri() . '/style.css',
@@ -12,6 +12,6 @@ function my_theme_enqueue_styles() {
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_directory_uri().'/dist/app.css',
 		array( $parenthandle ),
-		$theme->get( 'Version' ) // This only works if you have Version defined in the style header.
+		$theme->get( 'Version' )
 	);
 }
